624145eb2af21c74d1589c056dfcdf183e42fb52
PrettyyAll <45389298+PrettyyAll@users.noreply.github.com>
2018-12-11 00:40:55 +0800

    first

---
+++ zhulonghao/src/main/java/com/xiaozuanfeng/springcloud/Applications.java
@@ -0,0 +1,11 @@
+package com.xiaozuanfeng.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class Applications {
+	public static void main(String[] args) {
+		SpringApplication.run(Applications.class, args);
+	}
+}
