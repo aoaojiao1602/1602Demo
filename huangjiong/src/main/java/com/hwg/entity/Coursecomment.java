826edfc68045b9f41f69cabbba24f68cca2197d3
My_hjiong <912123784@qq.com>
2018-12-13 19:13:29 +0800

    黄炯 提交实体类

---
+++ huangjiong/src/main/java/com/hwg/entity/Coursecomment.java
@@ -0,0 +1,10 @@
+package com.hwg.entity;
+
+/**
+ * @Description 课程评价实体
+ * @author HJiong
+ * @time 2018年12月13日 下午7:09:35
+ */
+public class Coursecomment {
+
+}
---
+++ huangjiong/src/main/java/com/hwg/entity/Learnrecord.java
@@ -0,0 +1,10 @@
+package com.hwg.entity;
+
+/**
+ * @Description 学习记录实体
+ * @author HJiong
+ * @time 2018年12月13日 下午7:11:21
+ */
+public class Learnrecord {
+
+}
