1b06956d219e87614667541eb5b36845f24553b1
My_hjiong <912123784@qq.com>
2018-12-13 18:58:47 +0800

    黄炯 提交启动类

---
+++ huangjiong/src/main/java/com/hwg/Application.java
@@ -0,0 +1,15 @@
+package com.hwg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+
+@SpringCloudApplication // springBoot启动类
+public class Application {
+	   public static void main(String[] args) {
+	        SpringApplication.run(Application.class, args);
+	   }
+
+}
+ 
