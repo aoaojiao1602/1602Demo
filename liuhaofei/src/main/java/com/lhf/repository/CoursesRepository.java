48b9cbc117fb4e1e4b7a9a3e37a90718020e5d67
acer <acer@LAPTOP-TNADADED>
2018-12-13 17:45:04 +0800

    fhj

---
+++ liuhaofei/src/main/java/com/lhf/repository/CoursesRepository.java
@@ -0,0 +1,9 @@
+package com.lhf.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lhf.entity.Courses;
+
+public interface CoursesRepository extends JpaRepository<Courses, Integer>{
+
+}
