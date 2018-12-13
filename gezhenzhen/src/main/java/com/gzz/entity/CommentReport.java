b4ce039e744d26ba6100427dfb9386f9a93ffaf5
GWHH666 <18438258611@163.com>
2018-12-13 13:47:51 +0800

    hhh

--- gezhenzhen/src/main/java/com/gzz/entity/CommentReport.java
+++ gezhenzhen/src/main/java/com/gzz/entity/CommentReport.java
@@ -7,9 +7,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
@@ -20,8 +23,12 @@ public class CommentReport {
 	@Column(columnDefinition="int unsigned NOT NULL comment '备注：评论举报表自动增长主键'")
 	@JsonProperty(value ="id")
 	private Integer CommentReportId;
-	@Column(columnDefinition="int unsigned NOT NULL comment '备注：评论表id外键'")
-	private Integer CommentReportCommentId;
+	@JsonIgnore
+	@ManyToOne(targetEntity = ProjectReply.class)
+	@JoinColumn(name="CommentReportCommentId")
+	private Comment comment;
+	/*@Column(columnDefinition="int unsigned NOT NULL comment '备注：评论表id外键'")
+	private Integer CommentReportCommentId;*/
 	@Column(columnDefinition="int unsigned NOT NULL comment '备注：用户的id外键'")
 	private Integer CommentReportUid;
 	@Column(length=100)
--- gezhenzhen/src/main/java/com/gzz/entity/ProjectNotCount.java
+++ gezhenzhen/src/main/java/com/gzz/entity/ProjectNotCount.java
@@ -5,9 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
@@ -18,8 +21,12 @@ public class ProjectNotCount {
 	@Column(columnDefinition="int unsigned NOT NULL comment '备注：贴吧点踩自动增长主键'")
 	@JsonProperty(value = "id")
 	private Integer projectNotCountId;
-	@Column(columnDefinition="int unsigned NOT NULL comment '备注：贴吧表的id外键'")
-	private Integer projectNotCountProid;
+	@JsonIgnore
+	@ManyToOne(targetEntity = Project.class)
+	@JoinColumn(name="projectNotCountProjectId")
+	private Project project;
+	/*@Column(columnDefinition="int unsigned NOT NULL comment '备注：贴吧表的id外键'")
+	private Integer projectNotCountProid;*/
 	@Column(columnDefinition="int unsigned NOT NULL comment '备注：用户的id外键'")
 	private Integer projectNotCountUid;
 	@Transient
