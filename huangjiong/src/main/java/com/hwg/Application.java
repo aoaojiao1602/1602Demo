245484e2494c5dcacbd6e630f3b45dcb4c753087
My_hjiong <912123784@qq.com>
2018-12-14 00:16:40 +0800

    黄炯 -提交基本配置

--- huangjiong/src/main/java/com/hwg/Application.java
+++ huangjiong/src/main/java/com/hwg/Application.java
@@ -1,11 +1,15 @@
 package com.hwg;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
 
-@SpringCloudApplication // springBoot启动类
+/**
+ * @Descripti5n 项目启动类（sprintCloud）
+ * @author HJiong
+ * @time 2018年12月14日 上午12:14:49
+ */
+@SpringCloudApplication // sprintCloud启动类
 public class Application {
 	   public static void main(String[] args) {
 	        SpringApplication.run(Application.class, args);
