f4fea9b9c88cfcc8bbcbbc36d1a30acaf2950ad7
ZRDLQL0330 <18838053819@163.com>
2018-12-14 16:13:39 +0800

    提交实体类

--- huangjiong/src/main/java/com/hwg/entity/Coursecomment.java
+++ huangjiong/src/main/java/com/hwg/entity/Coursecomment.java
@@ -1,10 +1,42 @@
 package com.hwg.entity;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 /**
  * @Description 课程评价实体
  * @author HJiong
  * @time 2018年12月13日 下午7:09:35
  */
+@Entity
+@Table(name = "coursecommenttb")
 public class Coursecomment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(columnDefinition = "int unsigned NOT NULL comment '备注:课程评论表主键'")
+	private Integer commentId;
+	@Column(columnDefinition = "varchar(500) NOT NULL comment '备注:评价内容'")
+	private String commentContent;
+	@Column(columnDefinition = "int unsigned NOT NULL comment '备注:课程编号'")
+	private Integer commentCourseId;
+	@Column(columnDefinition = "int unsigned DEFAULT NULL comment '备注:评论被赞次数'")
+	private Integer commentLike;
+	@Column(columnDefinition = "int unsigned DEFAULT NULL comment '备注:评价星级'")
+	private Integer commentStar;
+	@Column(columnDefinition = "int unsigned NOT NULL comment '备注:学生编号'")
+	private Integer commentStuId;
+	@Column(columnDefinition = "timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '备注:评价时间'",nullable = false, insertable = false, updatable = false)
+	private Timestamp commentTime;
+	@Transient
+	private Integer filed1;
+	@Transient
+	private String filed2;
 
 }
--- huangjiong/src/main/java/com/hwg/entity/Learnrecord.java
+++ huangjiong/src/main/java/com/hwg/entity/Learnrecord.java
@@ -1,10 +1,37 @@
 package com.hwg.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 /**
  * @Description 学习记录实体
  * @author HJiong
  * @time 2018年12月13日 下午7:11:21
  */
+@Entity
+@Table(name = "learnrecordtb")
 public class Learnrecord {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(columnDefinition = "int unsigned NOT NULL comment '备注:学习记录表主键'")
+	private Integer lrId;
+	@Column(columnDefinition = "varchar(20) NOT NULL comment '备注:课程编号'")
+	private String learnCourseId;
+	@Column(columnDefinition = "varchar(200) NOT NULL comment '备注:学习进度'")
+	private String learnCourseRecord;
+	@Column(columnDefinition = "int unsigned NOT NULL comment '备注:视频进度记忆点'")
+	private Integer learnCourseTime;
+	@Column(columnDefinition = "varchar(200) NULL comment '备注:学习记录备注'")
+	private String learnRemark;
+	@Column(columnDefinition = "int unsigned NOT NULL comment '备注:学生编号'")
+	private Integer learnStuId;
+	@Transient
+	private Integer filed1;
+	@Transient
+	private String filed2;
 }
