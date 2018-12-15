cd178bcc2d1cd3303fda286abe0ce2a7447bfe36
燚星 <@LAPTOP-G4TFA8BL>
2018-12-15 17:38:34 +0800

    1

--- liuwenjin/src/main/java/com/lwj/springcloud/service/QuestionService.java
+++ liuwenjin/src/main/java/com/lwj/springcloud/service/QuestionService.java
@@ -1,5 +1,12 @@
 package com.lwj.springcloud.service;
 
-public interface QuestionService {
+import com.lwj.springcloud.entity.Question;
 
+public interface QuestionService {
+	//添加题库
+	public Question insertQuestion(Question q);
+	//删除题库
+	public int deleteQuestion(Integer qId);
+	//修改题库
+	public int updateQuestion(Question q);
 }
