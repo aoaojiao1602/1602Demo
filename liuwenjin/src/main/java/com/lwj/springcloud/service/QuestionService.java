201651692a7ed9c248440f3b4f813ec2cc9fd12a
燚星 <@LAPTOP-G4TFA8BL>
2018-12-15 17:42:42 +0800

    6

--- liuwenjin/src/main/java/com/lwj/springcloud/service/QuestionService.java
+++ liuwenjin/src/main/java/com/lwj/springcloud/service/QuestionService.java
@@ -1,12 +1,23 @@
 package com.lwj.springcloud.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.lwj.springcloud.entity.Entitysearch;
 import com.lwj.springcloud.entity.Question;
 
 public interface QuestionService {
-	//添加题库
-	public Question insertQuestion(Question q);
-	//删除题库
-	public int deleteQuestion(Integer qId);
-	//修改题库
-	public int updateQuestion(Question q);
+
+	
+	  /** 
+	 * http://localhost:8080/indexQuestionPage
+	  * 作者: 大娃   
+	  * 邮件: 1558936588@qq.com  
+	  * 时间: 2018年12月14日 下午11:49:03  
+	  * 版本: V1.0   
+	 */
+	
+	Page<Question> indexQuestionPage(Entitysearch search);
+
 }
