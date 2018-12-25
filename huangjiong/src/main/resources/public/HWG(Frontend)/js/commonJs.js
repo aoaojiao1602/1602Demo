8aac007f9a0445cd1bf5ba89d3d022de8581b476
My_hjiong <912123784@qq.com>
2018-12-25 23:44:47 +0800

    提交前端代码  更换springBoot版本

---
+++ huangjiong/src/main/resources/public/HWG(Frontend)/js/commonJs.js
@@ -0,0 +1,5 @@
+document.write('<script src="assets/js/jquery-1.12.3.min.js"></script>');
+document.write('<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>');
+document.write('<script type="text/javascript" src="assets/js/main.js"></script>');
+
+
