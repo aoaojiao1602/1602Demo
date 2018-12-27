e06c10b2260ff7e79cea24516a22058bdd575aac
My_hjiong <912123784@qq.com>
2018-12-27 14:36:44 +0800

    添加swagger

--- huangjiong/src/main/java/com/hwg/Application.java
+++ huangjiong/src/main/java/com/hwg/Application.java
@@ -3,12 +3,15 @@ package com.hwg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 /**
  * @Descripti5n 项目启动类（sprintCloud）
  * @author HJiong
  * @time 2018年12月14日 上午12:14:49
  */
+@EnableSwagger2
 @SpringCloudApplication // sprintCloud启动类
 public class Application {
 	   public static void main(String[] args) {
