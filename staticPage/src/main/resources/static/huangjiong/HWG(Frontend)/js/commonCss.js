a7ae01df1636555219c17bffc5b6d778a4f78003
My_hjiong <912123784@qq.com>
2019-01-03 14:10:00 +0800

    黄炯 前端

---
+++ staticPage/src/main/resources/static/huangjiong/HWG(Frontend)/js/commonCss.js
@@ -0,0 +1,7 @@
+document.write('<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600" rel="stylesheet">');
+document.write('<link href="assets/css/bootstrap.min.css" rel="stylesheet">');
+/*document.write('<link href="assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">');*/
+
+document.write('<link href="assets/css/base.css" rel="stylesheet">');
+document.write('<link href="assets/css/style.css" rel="stylesheet">');
+document.write('<link href="assets/vendors/linearicons/css/linearicons.css" rel="stylesheet">');
---
+++ staticPage/src/main/resources/static/huangjiong/HWG(Frontend)/js/commonJs.js
@@ -0,0 +1,57 @@
+//公共js文件
+document.write('<script src="assets/js/jquery-1.12.3.min.js"></script>');
+document.write('<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>');
+document.write('<script type="text/javascript" src="assets/js/main.js"></script>');
+
+
+
+//HWG公用js方法
+const Hwg = {
+	//设置token
+	setToken (thisToken){
+		window.sessionStorage.setItem("token",thisToken);
+	},
+	//获取token
+	getToken (){ 
+		return window.sessionStorage.getItem("token");
+	},
+	setOutToken (outToken){
+		window.sessionStorage.setItem("outToken",outToken);
+	},
+	getOutToken (){ 
+		return window.sessionStorage.getItem("outToken");
+	},
+	setUserId(uId){
+		window.sessionStorage.setItem("uId",uId);
+	},
+	getUserId(){
+		return window.sessionStorage.getItem("uId");
+	},
+	//获取地址栏参数
+	getUrlKey(name) {
+		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.href) || [, ""])[
+			1].replace(/\+/g, '%20')) || null
+	}
+	
+}
+//页面加载调用
+
+	if(Hwg.getToken()==null||Hwg.getToken()=='null'||Hwg.getToken==''){
+		Hwg.setToken(Hwg.getUrlKey("access_token"))//存入token
+		
+		Hwg.setOutToken(Hwg.getUrlKey("refresh_token"))//存入刷新token
+		
+		Hwg.setUserId(Hwg.getUrlKey("uid"))//存入用户id
+	}
+	//用户推出 清除所有信息
+	function outUser(url){
+		
+		$.post("http://localhost:8006/login/userEndLogin",{refresh_token:Hwg.getOutToken()},function(res){
+			
+		})
+		window.sessionStorage.removeItem("token");
+		window.sessionStorage.removeItem("uId")
+		window.sessionStorage.removeItem("outToken")
+		
+		window.location.href=url
+	}
