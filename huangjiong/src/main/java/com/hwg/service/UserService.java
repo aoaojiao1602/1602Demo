7876428feb0c0ef66e8bedc31ee77dce3081a054
My_hjiong <912123784@qq.com>
2019-01-03 09:38:36 +0800

    提交user接口

---
+++ huangjiong/src/main/java/com/hwg/service/UserService.java
@@ -0,0 +1,21 @@
+package com.hwg.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Description 调用朱龙豪组服务接口
+ * @author HJiong
+ * @time 2019年1月3日 上午12:36:10
+ */
+@FeignClient("ZHULONGHAO")
+public interface UserService {
+	
+	/**
+	 * 查询出用户信息
+	 * @return
+	 */
+	@RequestMapping("user/getUserId")
+	public Object queryUser (@RequestParam("uid")Integer uid);
+}
