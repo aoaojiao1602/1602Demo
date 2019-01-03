6e1dbf62f27b274baa5ec17900ed5467b52da846
My_hjiong <912123784@qq.com>
2019-01-03 09:58:00 +0800

    修改分类接口

--- huangjiong/src/main/java/com/hwg/service/CategoryService.java
+++ huangjiong/src/main/java/com/hwg/service/CategoryService.java
@@ -1,24 +1,11 @@
 package com.hwg.service;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 /**
- * @Description 分类业务接口 利用feign调用外部接口
+ * @Description 分类业务接口
  * @author HJiong
  * @time 2018年12月14日 下午1:56:32
  */
-@FeignClient("yongwenzheng")
 public interface CategoryService {
 
-	/**
-	 * 	查询出所有的类别信息 
-	 * @return
-	 */
-	@RequestMapping("navigation/getALLNavigationList")
-	public List<Map<String, Object>> getCategoryAll(@RequestParam("access_token") String access_token);
+
 }
